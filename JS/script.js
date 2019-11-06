2475fa7c4aae5597ae7e25c98577cc40ef6a41e7
Tygo Willems <tawillems@hotmail.com>
2019-11-06 11:44:01 +0100

    4de Commit

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+function myFunction(x) {
+    x.classList.toggle("fa-thumbs-down");
+}
+window.onload = setInterval(clock,1000);
+    function clock()
+    {
+        var d = new Date();
+        var date = d.getDate();
+        var year = d.getFullYear();
+        var month = d.getMonth();
+        var monthArr = ["January", "February","March", "April", "May", "June", "July", "August", "September", "October", "November","December"];
+        month = monthArr[month];
+        document.getElementById("date").innerHTML=date+" "+month+", "+year;
+    }
