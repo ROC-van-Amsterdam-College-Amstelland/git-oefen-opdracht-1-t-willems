0a35934bba157ae72a8ae42959db6ece194edf45
Tygo Willems <tawillems@hotmail.com>
2019-11-06 13:17:36 +0100

    FINAL COMMIT

--- JS/script.js
+++ JS/script.js
@@ -12,3 +12,15 @@ window.onload = setInterval(clock,1000);
         month = monthArr[month];
         document.getElementById("date").innerHTML=date+" "+month+", "+year;
     }
+function changeImage1(){
+    document.getElementById("image").src = "Images/foto 1.jpg";
+}
+function changeImage2(){
+    document.getElementById("image").src = "Images/foto 2.jpg";
+}
+function changeImage3(){
+    document.getElementById("image").src = "Images/foto 3.jpg";
+}
+function changeImage4(){
+    document.getElementById("image").src = "Images/foto 4.jpg";
+}
